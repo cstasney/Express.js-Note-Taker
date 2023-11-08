081c831a0deedbbbda67c15daa5424c1b538c96b
cstasney <cstasney07@gmail.com>
2023-11-07 20:01:34 -0500

    server.js finished, dependencies and router for htmlRoutes.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Imports and dependencies
 const express = require('express')
-const path = require('path')
-const api = require('./routes/index.js')
+// const path = require('path')
+const apiRoutes = require('./routes/apiRoutes.js')
 
 // Intialize Express.js and specify port
 const PORT = prcoess.env.PORT || 3001;
-const app =express();
+const app = express();
 
 // static middleware
 app.use(express.static('public'));
@@ -16,3 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes)
+
+// Listen for port
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+)
